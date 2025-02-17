805f71c8341097317f99d43aecfe784a24f5c648
maneesh-settipeta <maneeshsettipeta@gmail.com>
2025-02-16 18:24:29 -0600

    Testing

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ connection.connect(err => {
 
 app.get("/", async (req, res) => {
     try {
-        res.status(200).send("connection Success")
+        res.status(200).send("Connection Success...")
     } catch (error) {
         res.status(500).send("connection unsucessfull")
     }
@@ -243,7 +243,7 @@ ON
     blogs.useruuid = users.useruuid;`
         const blogs = await connection.query(query);
         if (blogs.rows.length > 0) {
-            res.status(200).json({ message: "User found", blogs: blogs.rows });
+            res.status(200).json({ message: "User found...", blogs: blogs.rows });
         }
         else {
             res.status(401).send("Failed to fetch test");
